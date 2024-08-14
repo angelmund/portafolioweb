b5fbe13353e06c56b1f3d48d1ec04b64d805764f
angelmund <97712695+angelmund@users.noreply.github.com>
2024-08-13 23:45:33 -0600

    Cambio nuevo

--- script.js
+++ script.js
@@ -42,3 +42,31 @@ function seleccionar(){
 // window.onscroll = function(){
 //     efectoHabilidades();
 // }
+
+document.addEventListener('DOMContentLoaded',function(event){
+    var dataText = [ "Hola, soy Miguel Angel", "Desarrollador Web", "Egresado de la Ingeniería en Sistemas"];
+          function typeWriter(text, i, fnCallback) {
+      if (i < (text.length)) {
+       document.querySelector(".text").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+            setTimeout(function() {
+          typeWriter(text, i + 3, fnCallback)
+        }, 500);
+      }
+      else if (typeof fnCallback == 'function') {
+        setTimeout(fnCallback, 10);
+      }
+    }
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 500);
+       }
+      if (i < dataText[i].length) {
+       typeWriter(dataText[i], 0, function(){
+         StartTextAnimation(i + 1);
+       });
+      }
+    }
+    StartTextAnimation(0);
+  });
